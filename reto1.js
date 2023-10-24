81925287a02161555887c4ecb6d4c764c09157de
Eva2626 <evoti1985@gmail.com>
2023-10-24 15:52:21 +0200

    subo reto1 de git

---
+++ reto1.js
@@ -0,0 +1,67 @@
+function calculadoraSuma(operacion, op1, op2) {
+
+    let resultadoSuma;
+
+    if (operacion == "sum") {
+        resultadoSuma = op1 + op2
+    }
+
+    return resultadoSuma
+}
+
+
+
+function calculadoraResta(operacion, op1, op2) {
+
+    let resultadoResta;
+
+    if (operacion == "subst") {
+        resultadoResta = op1 - op2
+    }
+
+    return resultadoResta
+}
+
+
+
+function calculadoraMultiplicacion(operacion, op1, op2) {
+
+    let resultadoMult;
+
+    if (operacion == "mult") {
+        resultadoMult = op1 * op2
+    }
+
+    return resultadoMult
+}
+
+
+
+function calculadoraDivision(operacion, op1, op2) {
+
+    let resultadoDiv;
+
+    if (operacion == "div") {
+        resultadoDiv = op1 / op2
+    }
+
+    return resultadoDiv
+}
+
+function cuadrado(numero) {
+    let resultado = numero * numero
+    return resultado
+}
+
+function calculadoraCuadrado(operacion, op1) {
+
+    let resultadoCuadrado;
+
+    if (operacion == "cuadrado") {
+        resultadoCuadrado = op1^2//elevado a 2
+    }
+
+    return resultadoCuadrado
+}
+
+module.exports = { calculadoraSuma, calculadoraResta, calculadoraMultiplicacion, calculadoraDivision }
